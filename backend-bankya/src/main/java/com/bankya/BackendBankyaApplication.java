3cf866e3393f38b702fae40e3d9192ccb13f8fa0
Fabian <fabianyate31@gmail.com>
2022-01-04 23:54:28 -0500

    Proyecto creado

---
+++ backend-bankya/src/main/java/com/bankya/BackendBankyaApplication.java
@@ -0,0 +1,13 @@
+package com.bankya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendBankyaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendBankyaApplication.class, args);
+	}
+
+}
---
+++ backend-bankya/src/test/java/com/bankya/BackendBankyaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bankya;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendBankyaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
